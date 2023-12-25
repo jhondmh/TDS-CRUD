502a234601649c409f2730fdf6dfb21f7f3fd9c2
lolo <lolo@nan.net>
2023-12-25 12:02:38 -0500

    more ducks xd;

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -48,7 +48,7 @@ export default function Welcome({
                     href={route('register')}
                     className="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
                   >
-                    Registerse
+                    Registarme
                   </Link>
                 ) : null}
               </>
